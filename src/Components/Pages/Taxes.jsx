33ea2e12d13af3b711d7e5b2126faa45a8603424
AllanNastin <nastina@tcd.ie>
2022-08-14 18:34:46 +0100

    • Tax Calculations Visualise on Site

--- src/Components/Pages/Taxes.jsx
+++ src/Components/Pages/Taxes.jsx
@@ -3,18 +3,18 @@ import { useState } from "react";
 
 import "../CSS/Flex.css";
 
-var grossPay = 6;
+// var grossPay = 6;
 var taxCredit = 3400;
 var cutOff = 45800;
 var lowBand = 0.2;
 var highBand = 0.4;
-var grossPay = 0;
-var netPay = 0;
-var paye = 0;
-var usc = 0;
-var prsi = 0;
-var taxPay = 0;
-var percentage = 0;
+var tempGrossPay = 0;
+// var tempNetPay = 0;
+var tempPaye = 0;
+var tempUsc = 0;
+var tempPrsi = 0;
+// var tempTaxPay = 0;
+// var tempPercentage = 0;
 
 var uscCuttOff = 13000;
 var uscFirstBand = 12012;
@@ -24,53 +24,65 @@ var uscFirstTaxRate = 0.005;
 var uscSecondTaxRate = 0.02;
 var uscThirdTaxRate = 0.045;
 var uscFourthTaxRate = 0.08;
-var uscFifthTaxRate = 0.11;
+// var uscFifthTaxRate = 0.11;
 
 var prsiCutOff = 352 * 52;
 var prsiTaxRate = 0.04;
 
 export const Taxes = () => {
   const { t } = useTranslation();
+  const [netPay, setNetPay] = useState(0);
+  const [percentage, setPercentage] = useState(0);
+  const [grossPay, setGrossPay] = useState(0);
+  const [usc, setUsc] = useState(0);
+  const [prsi, setPrsi] = useState(0);
+  const [paye, setPaye] = useState(0);
+  const [taxPay, setTaxPay] = useState(0);
+  // const [grossPay, setGrossPay] = useState(0)
   //   const [netPay, setNetPay] = useState(0);
   const getInputValue = (event) => {
-    grossPay = event.target.value;
+    tempGrossPay = event.target.value;
     // console.log(grossPay);
   };
   const calculateTax = () => {
-    usc = 0;
-    prsi = 0;
+    setGrossPay(tempGrossPay);
+    tempUsc = 0;
+    tempPrsi = 0;
     // paye = 0;
-    if (grossPay <= cutOff) {
-      paye = grossPay * lowBand;
-    } else if (grossPay > cutOff) {
-      paye = cutOff * lowBand + (grossPay - cutOff) * highBand;
+    if (tempGrossPay <= cutOff) {
+      tempPaye = tempGrossPay * lowBand;
+    } else if (tempGrossPay > cutOff) {
+      tempPaye = cutOff * lowBand + (tempGrossPay - cutOff) * highBand;
     }
-    if (paye <= taxCredit) {
-      paye = 0;
+    if (tempPaye <= taxCredit) {
+      tempPaye = 0;
     } else {
-      paye -= taxCredit;
+      tempPaye -= taxCredit;
     }
-    if (grossPay > uscCuttOff) {
-      usc = uscFirstBand * uscFirstTaxRate;
-      if (grossPay > uscSecondBand) {
-        usc += (uscSecondBand - uscFirstBand) * uscSecondTaxRate;
+    if (tempGrossPay > uscCuttOff) {
+      tempUsc = uscFirstBand * uscFirstTaxRate;
+      if (tempGrossPay > uscSecondBand) {
+        tempUsc += (uscSecondBand - uscFirstBand) * uscSecondTaxRate;
       } else {
-        usc += (grossPay - uscFirstBand) * uscSecondTaxRate;
+        tempUsc += (grossPay - uscFirstBand) * uscSecondTaxRate;
       }
-      if (grossPay > uscThirdBand) {
-        usc += (uscThirdBand - uscSecondBand) * uscThirdTaxRate;
-        usc += (grossPay - uscThirdBand) * uscFourthTaxRate;
-      } else if (grossPay > uscSecondBand) {
-        usc += (grossPay - uscSecondBand) * uscThirdTaxRate;
+      if (tempGrossPay > uscThirdBand) {
+        tempUsc += (uscThirdBand - uscSecondBand) * uscThirdTaxRate;
+        tempUsc += (tempGrossPay - uscThirdBand) * uscFourthTaxRate;
+      } else if (tempGrossPay > uscSecondBand) {
+        tempUsc += (tempGrossPay - uscSecondBand) * uscThirdTaxRate;
       }
     }
 
-    if (grossPay > prsiCutOff) {
-      prsi = grossPay * prsiTaxRate;
+    if (tempGrossPay > prsiCutOff) {
+      tempPrsi = tempGrossPay * prsiTaxRate;
     }
-    taxPay = paye + prsi + usc;
-    netPay = grossPay - taxPay;
-    percentage = (taxPay / grossPay) * 100;
+    setPrsi(tempPrsi);
+    setUsc(tempUsc);
+    setPaye(tempPaye);
+    setTaxPay(tempPaye + tempPrsi + tempUsc);
+    setNetPay(tempGrossPay - tempPaye + tempPrsi + tempUsc);
+    setPercentage(((tempPaye + tempPrsi + tempUsc) / tempGrossPay) * 100);
 
     console.log(
       "Gross Pay: €" +
@@ -84,10 +96,42 @@ export const Taxes = () => {
         "\n USC: €" +
         usc +
         "\nPercentage: " +
-        percentage + "%" +
+        percentage +
+        "%" +
         "\nNet pay: €" +
         netPay
     );
+
+    // <console className="logf">"Gross Pay: €" +
+    // grossPay +
+    // "\nTaxes due: €" +
+    // taxPay +
+    // "\n PAYE: €" +
+    // paye +
+    // "\n PRSI: €" +
+    // prsi +
+    // "\n USC: €" +
+    // usc +
+    // "\nPercentage: " +
+    // percentage + "%" +
+    // "\nNet pay: €" +
+    // netPay</console>
+    // (
+    //   "Gross Pay: €" +
+    //     grossPay +
+    //     "\nTaxes due: €" +
+    //     taxPay +
+    //     "\n PAYE: €" +
+    //     paye +
+    //     "\n PRSI: €" +
+    //     prsi +
+    //     "\n USC: €" +
+    //     usc +
+    //     "\nPercentage: " +
+    //     percentage + "%" +
+    //     "\nNet pay: €" +
+    //     netPay
+    // );
   };
 
   return (
@@ -96,7 +140,28 @@ export const Taxes = () => {
       {/* <input type="submit" value={t("calculate_button")} /> */}
       <br />
       {/* take user input and print it */}
-      <button onClick={calculateTax}>{t("calculate_button")}</button>
+      <button
+        onClick={() => {
+          calculateTax();
+        }}
+      >
+        {t("calculate_button")}
+      </button>
+      <p>
+        Gross Pay: {grossPay}
+        <br />
+        Taxes Due: {taxPay}
+        <br />
+        Paye: {paye}
+        <br />
+        PRSI: {prsi}
+        <br />
+        USC: {usc}
+        <br />
+        Percentage: {percentage}
+        <br />
+        NetPay: {netPay}
+      </p>
       {/* <span>{netPay}</span> */}
       {/* <input type={"text"} onChange={getInputValue}/> */}
       {/* <button onClick={getInputValue}>Calculate Taxes newNumber: {newNumber}</button> */}
